Fixed softmax bug in inference mode. Added encodePositionForInference and decodeOutput to encode.py. Switched from saving the entire model to just saving the state_dict in train.py. Created visualize.py

AlphaZeroNetwork.py
@@ -170,7 +170,7 @@ class AlphaZeroNet( nn.Module ):
             x (torch.Tensor) the input tensor.
             valueTarget (torch.Tensor) the value target.
             policyTarget (torch.Tensor) the policy target.
-            policyMask (torch.Tensor) the mask for the legal moves.
+            policyMask (torch.Tensor) the legal move mask
         """
 
         x = self.convBlock1( x )
@@ -196,9 +196,15 @@ class AlphaZeroNet( nn.Module ):
 
             policyMask = policyMask.view( policyMask.shape[0], -1 )
 
-            policy = torch.where( torch.eq( policyMask, 1 ), policy, torch.zeros( policy.shape ).cuda() )
+            policy_max = torch.max( policy, dim=1, keepdim=True)[0]
 
-            policy = self.softmax1( policy )
+            policy_exp = torch.exp( policy - policy_max )
 
-            return value, policy
+            policy_exp *= policyMask.type( torch.float32 )
+
+            policy_exp_sum = torch.sum( policy_exp, dim=1, keepdim=True )
+            
+            policy_softmax = policy_exp / policy_exp_sum
+
+            return value, policy_softmax
 

train.py
@@ -8,8 +8,8 @@ from CCRLDataset import CCRLDataset
 from AlphaZeroNetwork import AlphaZeroNet
 
 num_epochs = 100
-num_blocks = 20
-num_filters = 256
+num_blocks = 5
+num_filters = 64
 
 def train():
 
@@ -23,7 +23,7 @@ def train():
 
     test_ds = CCRLDataset( ccrl_test_dir )
 
-    train_batch_size = 64
+    train_batch_size = 1024
 
     test_batch_size = 4096
 
@@ -126,9 +126,9 @@ def train():
 
         networkFileName = 'AlphaZeroNet_{}x{}_{}.pt'.format( num_blocks, num_filters, epoch ) 
 
-        torch.save( alphaZeroNet, networkFileName )
+        torch.save( alphaZeroNet.state_dict(), networkFileName )
 
-        print( 'Saved network to {}'.format( networkFileName ) )
+        print( 'Saved model to {}'.format( networkFileName ) )
 
 if __name__ == '__main__':
 